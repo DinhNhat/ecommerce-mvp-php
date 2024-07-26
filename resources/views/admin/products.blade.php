38b9dfeff193f989c5b8a0da3a6c745a62df3b50
LAPTOP-RD55DV0J\Nat <nguyendinhnhat206@gmail.com>
2024-07-26 09:56:36 -0400

    -- Added dropdown content for products page

--- resources/views/admin/products.blade.php
+++ resources/views/admin/products.blade.php
@@ -16,6 +16,34 @@
         }
 
         tr:nth-child(even) {background-color: #f2f2f2;}
+
+        .dropdown {
+            position: relative;
+            display: inline-block;
+        }
+
+        .dropbtn:hover, .dropbtn:focus {
+            background-color: rgb(241, 236, 236);
+        }
+
+        .dropdown-content {
+            display: none;
+            position: absolute;
+            background-color: #f1f1f1;
+            min-width: 160px;
+            overflow: auto;
+            box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+            z-index: 1;
+        }
+
+        .dropdown-content a {
+            color: black;
+            padding: 12px 16px;
+            text-decoration: none;
+            display: block;
+        }
+
+        .dropdown a:hover {background-color: #ddd;}
     </style>
 
     <div class="w-75 d-flex justify-content-between align-items-center">
@@ -33,6 +61,7 @@
             <th>Name</th>
             <th>Price</th>
             <th>Orders</th>
+            <th></th>
         </tr>
 
         @if ($products->count() > 0)
@@ -42,6 +71,14 @@
                     <td>{{ $product->name }}</td>
                     <td>{{ \Illuminate\Support\Number::currency(($product->price_in_cents / 100), 'CAD')  }}</td>
                     <td>{{ $product->orders_count }}</td>
+                    <td>
+                        <i class="bi bi-three-dots-vertical dropbtn" style="font-size: 1.2rem; color: black; cursor: pointer"></i>
+                        <div id="drop-download{{ $product->id }}" class="dropdown-content">
+                            <a href="#home">Home</a>
+                            <a href="#about">About</a>
+                            <a href="#contact">Contact</a>
+                        </div>
+                    </td>
                 </tr>
             @endforeach
         @else
@@ -50,5 +87,15 @@
             </tr>
         @endif
     </table>
+
+    <script>
+        $(function() {
+            $(".dropbtn").each(function() {
+                $(this).on("click", function() {
+                    
+                });
+            });
+        });
+    </script>
 
 @endsection
